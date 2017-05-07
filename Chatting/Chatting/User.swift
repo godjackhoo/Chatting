10e70c8a6dcebf2e9364e1dbf740b76fd4c1ec66
༼☠͙ JåćK ✩ Ħᴓᴓ ☠͙༽ <hotranhuuhieu@gmail.com>
2017-05-08 03:33:40 +0700

    Chatting App Final

---
+++ Chatting/Chatting/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Chatting
+//
+//  Created by ༼☠͙ JåćK ✩ Ħᴓᴓ ☠͙༽ on 1/20/17.
+//  Copyright © 2017 ༼☠͙ JåćK ✩ Ħᴓᴓ ☠͙༽. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var id: String?
+    var name: String?
+    var email: String?
+    var profileImageUrl: String?
+}
